dc34c29b1db8a5127e0e67f1464038b0b35ec737
Nikddimos Gebregiorgis <gebrenick07@gmail.com>
2019-12-16 09:57:39 -0700

    updating quiz

---
+++ script.js
@@ -0,0 +1,26 @@
+var questions = [
+    {
+      title: "Commonly used data types DO NOT include:",
+      choices: ["strings", "booleans", "alerts", "numbers"],
+      answer: "alerts"
+    },
+    {
+      title: "The condition in an if / else statement is enclosed within ____.",
+      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+      answer: "parentheses"
+    },
+    {
+        title: "What dose HTML stand for",
+        choices: ["hypertext markup language", "cascading style sheets", "parentheses", "java script"],
+        answer: "hypertext markup language"
+    },
+    {
+        title: "who is on the two dollar bill"
+        choices: ["Abraham Lincoln", "Dwight D. Eisenhower", "Thomas Jefferson", "Albert Einstein"],
+        answer: "Thomas Jefferson"
+    },
+    {
+        title: "What dose CSS stand for",
+        choices: ["strings", "java script", "cascading style sheets", "square brackets"],
+        answer: "cascading style sheets"
+    };
