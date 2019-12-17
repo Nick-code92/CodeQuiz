0b89dee5616738113e23e319de6b22d0952c0cbb
Nikddimos Gebregiorgis <gebrenick07@gmail.com>
2019-12-17 00:20:56 -0700

    updating file

--- script.js
+++ script.js
@@ -2,33 +2,38 @@ function () {
 
 var questions = [
     {
-      title: "Commonly used data types DO NOT include:",
+      title: "Commonly used data types DO NOT include?",
       choices: ["strings", "booleans", "alerts", "numbers"],
       answer: "alerts",
     },
     {
-      title: "The condition in an if / else statement is enclosed within ____.",
+      title: "The condition in an if / else statement is enclosed within ____.?",
       choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
       answer: "parentheses",
     },
     {
-        title: "What dose HTML stand for",
+        title: "What dose HTML stand for?",
         choices: ["hypertext markup language", "cascading style sheets", "parentheses", "java script"],
         answer: "hypertext markup language",
     },
     {
-        title: "who is on the two dollar bill",
+        title: "who is on the two dollar bill?",
         choices: ["Abraham Lincoln", "Dwight D. Eisenhower", "Thomas Jefferson", "Albert Einstein"],
         answer: "Thomas Jefferson",
     },
     {
-        title: "What dose CSS stand for",
+        title: "What dose CSS stand for?",
         choices: ["strings", "java script", "cascading style sheets", "square brackets"],
         answer: "cascading style sheets",
     },
     {
-      title: "Web pages are written using",
+      title: "Web pages are written using?",
       choices: ["CSS", "JS", "HTML", "URL"],
       answer: "HTML",
+    },
+    {
+      title: "Which is a single integrated circuit?",
+      choices: ["Mother Board", "Gate", "CPU", "Chip"],
+      answer: "Gate",
     }
   });
