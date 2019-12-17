14e56ee12a468799e48390ca89101a4528fb20aa
Nikddimos Gebregiorgis <gebrenick07@gmail.com>
2019-12-16 23:20:58 -0700

    upading file

--- script.js
+++ script.js
@@ -1,26 +1,34 @@
+function () {
+
 var questions = [
     {
       title: "Commonly used data types DO NOT include:",
       choices: ["strings", "booleans", "alerts", "numbers"],
-      answer: "alerts"
+      answer: "alerts",
     },
     {
       title: "The condition in an if / else statement is enclosed within ____.",
       choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-      answer: "parentheses"
+      answer: "parentheses",
     },
     {
         title: "What dose HTML stand for",
         choices: ["hypertext markup language", "cascading style sheets", "parentheses", "java script"],
-        answer: "hypertext markup language"
+        answer: "hypertext markup language",
     },
     {
-        title: "who is on the two dollar bill"
+        title: "who is on the two dollar bill",
         choices: ["Abraham Lincoln", "Dwight D. Eisenhower", "Thomas Jefferson", "Albert Einstein"],
-        answer: "Thomas Jefferson"
+        answer: "Thomas Jefferson",
     },
     {
         title: "What dose CSS stand for",
         choices: ["strings", "java script", "cascading style sheets", "square brackets"],
-        answer: "cascading style sheets"
-    };
+        answer: "cascading style sheets",
+    },
+    {
+      title: "Web pages are written using",
+      choices: ["CSS", "JS", "HTML", "URL"],
+      answer: "HTML",
+    }
+  });
